5a750a5df9115ee01ff9c1905e25613782da0de5
kierancyphus <kierancyphus@gmail.com>
2021-11-23 16:51:46 -0500

    basic gateway code

--- gateway/app.py
+++ gateway/app.py
@@ -1,8 +1,30 @@
-from flask import Flask
+from flask import Flask, request
+import logging
+from utils import *
 
+logging.getLogger().setLevel(logging.INFO)
 app = Flask(__name__)
 
 
 @app.route('/ping')
 def ping() -> str:
     return "pong"
+
+
+@app.route('/get-anatomical-region', methods=["POST"])
+def get_anatomical_region() -> str:
+    """
+
+    :return: will be one of the regions specified in x file, or an error
+    """
+    # data should be a dictionary
+    data = dict(request.form)
+    app.logger.info(f"data: {data}")
+
+    # take data and send it to get preprocessed
+    data = preprocess_data()
+
+    # send it to model
+    result = perform_inference()
+
+    return stringify_model_output(result)
---
+++ gateway/utils.py
@@ -0,0 +1,10 @@
+def preprocess_data():
+    pass
+
+
+def perform_inference():
+    pass
+
+
+def stringify_model_output(result) -> str:
+    pass
