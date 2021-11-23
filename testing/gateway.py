21f11a8b584135c7cbdfac2eeade25c3213338f3
kierancyphus <kierancyphus@gmail.com>
2021-11-23 16:52:14 -0500

    testing files for gateway. These should be changed into something more robust later.

---
+++ testing/gateway.py
@@ -0,0 +1,14 @@
+import requests
+import os
+
+
+if __name__ == "__main__":
+    os.environ['NO_PROXY'] = '127.0.0.1'
+    response = requests.get("http://localhost:5000/ping")
+    print(response.text)
+
+    url = "http://127.0.0.1:5000/get-anatomical-region"
+    data = {'some': 'test', 'data': 'here'}
+
+    response = requests.post(url, data)
+    print(response.text)
